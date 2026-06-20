efb9e2241f180e10c10060bd8a96cb25f9637e9c
coffeinaisthebest <coffeinaisthebest@gmail.com>
2026-06-20 18:26:35 -0300

    Update ordemCor.py

--- ordemCor.py
+++ ordemCor.py
@@ -1,26 +1,148 @@
 import pickle
-#Entrada de Dados
 
 def chamado():
     return input("Digite o Nome do Arquivo:\t")
 
-#Processamento de Dados
+arq = chamado()
+#leitura do arquivo binário:
+with open(arq, 'rb') as arquivo:
+    usuarios = pickle.load(arquivo)
+    veiculos = pickle.load(arquivo)
+    corridas = pickle.load(arquivo)
 
-def main():
-    
-    lcorrida = []
-    arq = chamado();
-    with open(arq , "rb") as f:
-        usuarios = pickle.load(f)
-        veiculos = pickle.load(f)
-        corridas = pickle.load(f)
 
-    for n in corridas:
-        lcorrida.append(n)
-        
-        
-        
+"""
+(Ah, queria deixar algo de zero relação com o código: to pensando fortemente
+em substituir parte dos meus comentários com HASHTAG por essa formatação aqui, ta.
+É muito legal o fato de que eu posso minimizar o comentário dessa forma. Anyway, voltando para a luta:)
+"""
 
+#criação da lista de ids (somente as chaves do dicionário corridas):
+ids_corridas = []
+
+for elem in corridas:
+    ids_corridas.append(elem)
+
+
+#------------------------------------------------------------
+
+
+#mapeamento de prioridade das categorias:
+categoria_prioridade = {'Black': 1,'Comfort': 2,'Comum': 3,'Moto': 4}
+
+
+#-----------------------------------------------------------------
+
+
+#função de comparação (CRITÉRIOS)
+def comparar(id1, id2):
+    """
+    Retorna True se id1 deve vir antes de id2 na ordenação.
+    Critérios (em ordem de prioridade):
+      1. Categoria (Black > Comfort > Comum > Moto)
+      2. Data (mais recente primeiro)
+      3. Estrelas do motorista (maior primeiro)
+      4. Nome do cliente (ordem alfabética)
+      5. Valor da corrida (maior primeiro)
+    """
+
+    #desempacotamento do dicionario corridas
+    placa1, cpf_cliente1, data1, horario1, duracao1, valor1 = corridas[id1]
+    placa2, cpf_cliente2, data2, horario2, duracao2, valor2 = corridas[id2]
+
+
+    #desempacotamento de data
+    dia1, mes1, ano1 = data1
+    dia2, mes2, ano2 = data2
+
+    #1.Categoria
+    cat1 = veiculos[placa1][0]
+    cat2 = veiculos[placa2][0]
+    prio1 = categoria_prioridade[cat1]
+    prio2 = categoria_prioridade[cat2]
+
+    if prio1 != prio2:
+        return prio1 < prio2
+
+
+    #2.Data (mais recente primeiro)
+
+    #comparar ano, mês, dia de forma decrescente
+    if ano1 != ano2:
+        return ano1 > ano2
+    if mes1 != mes2:
+        return mes1 > mes2
+    if dia1 != dia2:
+        return dia1 > dia2
+
+    #3.Estrelas do motorista (maior primeiro)
+    cpf_motorista1 = veiculos[placa1][1]
+    cpf_motorista2 = veiculos[placa2][1]
+
+    #desempacota os dados do motorista (nome, estrelas, status)
+    _, estrelas1, _ = usuarios[cpf_motorista1]
+    _, estrelas2, _ = usuarios[cpf_motorista2]
+
+    if estrelas1 != estrelas2:
+        return estrelas1 > estrelas2
+
+    #4.Nome do cliente (ordem alfabética)
+    nome_cliente1, _, _ = usuarios[cpf_cliente1]
+    nome_cliente2, _, _ = usuarios[cpf_cliente2]
+
+    if nome_cliente1 != nome_cliente2:
+        return nome_cliente1 < nome_cliente2
+
+    #5.Valor da corrida (maior primeiro)
+    return valor1 > valor2
+
+
+#-----------------------------------------------------------------
+
+
+
+def merge(l, lEsq, lDir):
+    i = 0  #ponteiro para lEsq
+    j = 0  #ponteiro para lDir
+    k = 0  #ponteiro para l (resultado)
+
+    while i < len(lEsq) and j < len(lDir):
+        if comparar(lEsq[i], lDir[j]):   
+            l[k] = lEsq[i]
+            i += 1
+        else:
+            l[k] = lDir[j]
+            j += 1
+        k += 1
+
+    #copia o restante de lEsq
+    while i < len(lEsq):
+        l[k] = lEsq[i]
+        i += 1
+        k += 1
+
+    #copia o restante de lDir 
+    while j < len(lDir):
+        l[k] = lDir[j]
+        j += 1
+        k += 1
+
+
+def mergeSort(l):
+    if len(l) > 1:
+        meio = len(l) // 2
+        lEsq = l[:meio]      #cópia da primeira metade
+        lDir = l[meio:]      #cópia da segunda metade
+
+        mergeSort(lEsq)
+        mergeSort(lDir)
+
+        merge(l, lEsq, lDir)
+
+
+#EXECUÇÃO
+mergeSort(ids_corridas)
+print(ids_corridas)
 
 #Estrutura dos dicionários:
 # usuarios = {’468.791.579-55’: (’Lucas Soares Lopes’, 1, False),
@@ -37,8 +159,6 @@ def main():
 
 
 #Estrutura de como deve ser a saída
-
-    for c in lcorrida:
         
 '''
 CATEGORIA: Black
