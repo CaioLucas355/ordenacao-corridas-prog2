001b4eefa45d47bc44012858c33c3ed458e5d5a0
Cailu Caz <caiolucas.sn@gmail.com>
2026-06-25 18:02:08 -0300

    update2506
    renomeação de arquivos

--- ordemCor_ancestral.py
+++ ordemCor_ancestral.py
@@ -1,15 +1,8 @@
-#ARTHUR CORRÊA SOBRINHO 20252BSI0046
-#CAIO LUCAS DA SILVA NASCIMENTO 20252BSI007
-
-#--------------------------------------------------------------------------
-
 import pickle
 from time import time
 
-
 def chamado():
     return input("Digite o Nome do Arquivo:\t")
-
 
 arq = chamado()
 #leitura do arquivo binário:
@@ -19,8 +12,20 @@ with open(arq, 'rb') as arquivo:
     corridas = pickle.load(arquivo)
 
 
+"""
+(Ah, queria deixar algo de zero relação com o código: to pensando fortemente
+em substituir parte dos meus comentários com HASHTAG por essa formatação aqui, ta.
+É muito legal o fato de que eu posso minimizar o comentário dessa forma. Anyway, voltando para a luta:)
+"""
+
 #criação da lista de ids (somente as chaves do dicionário corridas):
-ids_corridas = [elem for elem in corridas]
+ids_corridas = []
+
+for elem in corridas:
+    ids_corridas.append(elem)
+
+
+#------------------------------------------------------------
 
 
 #mapeamento de prioridade das categorias:
@@ -28,6 +33,9 @@ categoria_prioridade = {'Black': 1,'Comfort': 2,'Comum': 3,'Moto': 4}
 
 
 #-----------------------------------------------------------------
+
+
+#função de comparação (CRITÉRIOS)
 def comparar(id1, id2):
     """
     Retorna True se id1 deve vir antes de id2 na ordenação.
@@ -38,18 +46,29 @@ def comparar(id1, id2):
       4. Nome do cliente (ordem alfabética)
       5. Valor da corrida (maior primeiro)
     """
+
+    #desempacotamento do dicionario corridas
     placa1, cpf_cliente1, data1, horario1, duracao1, valor1 = corridas[id1]
     placa2, cpf_cliente2, data2, horario2, duracao2, valor2 = corridas[id2]
+
+
+    #desempacotamento de data
     dia1, mes1, ano1 = data1
     dia2, mes2, ano2 = data2
 
+    #1.Categoria
     cat1 = veiculos[placa1][0]
     cat2 = veiculos[placa2][0]
     prio1 = categoria_prioridade[cat1]
     prio2 = categoria_prioridade[cat2]
+
     if prio1 != prio2:
         return prio1 < prio2
 
+
+    #2.Data (mais recente primeiro)
+
+    #comparar ano, mês, dia de forma decrescente
     if ano1 != ano2:
         return ano1 > ano2
     if mes1 != mes2:
@@ -57,43 +76,39 @@ def comparar(id1, id2):
     if dia1 != dia2:
         return dia1 > dia2
 
+    #3.Estrelas do motorista (maior primeiro)
     cpf_motorista1 = veiculos[placa1][1]
     cpf_motorista2 = veiculos[placa2][1]
+
+    #desempacota os dados do motorista (nome, estrelas, status)
     _, estrelas1, _ = usuarios[cpf_motorista1]
     _, estrelas2, _ = usuarios[cpf_motorista2]
+
     if estrelas1 != estrelas2:
         return estrelas1 > estrelas2
 
+    #4.Nome do cliente (ordem alfabética)
     nome_cliente1, _, _ = usuarios[cpf_cliente1]
     nome_cliente2, _, _ = usuarios[cpf_cliente2]
+
     if nome_cliente1 != nome_cliente2:
         return nome_cliente1 < nome_cliente2
 
+    #5.Valor da corrida (maior primeiro)
     return valor1 > valor2
 
 
 #-----------------------------------------------------------------
 
-def insertion_sort(l, inicio, fim):
 
-    for i in range(inicio + 1, fim):
-        chave = l[i]
-        j = i - 1
-        while j >= inicio and comparar(chave, l[j]):
-            l[j + 1] = l[j]
-            j -= 1
-        l[j + 1] = chave
-
-#-----------------------------------------------------------------
 
 def merge(l, lEsq, lDir):
-
     i = 0  #ponteiro para lEsq
     j = 0  #ponteiro para lDir
     k = 0  #ponteiro para l (resultado)
 
     while i < len(lEsq) and j < len(lDir):
-        if comparar(lEsq[i], lDir[j]):
+        if comparar(lEsq[i], lDir[j]):   
             l[k] = lEsq[i]
             i += 1
         else:
@@ -101,13 +116,11 @@ def merge(l, lEsq, lDir):
             j += 1
         k += 1
 
-
     #copia o restante de lEsq
     while i < len(lEsq):
         l[k] = lEsq[i]
         i += 1
         k += 1
-
 
     #copia o restante de lDir 
     while j < len(lDir):
@@ -116,59 +129,53 @@ def merge(l, lEsq, lDir):
         k += 1
 
 
-def merge_sortCA(l):
+def mergeSort(l):
+    if len(l) > 1:
+        meio = len(l) // 2
+        lEsq = l[:meio]      #cópia da primeira metade
+        lDir = l[meio:]      #cópia da segunda metade
 
-    if len(l) <= 30:
-        insertion_sort(l, 0, len(l))
-        return
-    
-    meio = len(l) // 2
-    lEsq = l[:meio] #cópia da primeira metade
-    lDir = l[meio:] #cópia da segunda metade
-    
-    merge_sortCA(lEsq)
-    merge_sortCA(lDir)
-    merge(l, lEsq, lDir)
+        mergeSort(lEsq)
+        mergeSort(lDir)
 
-#-----------------------------------------------------------------------------------
+        merge(l, lEsq, lDir)
+
 
 #EXECUÇÃO
 t1 = time()
-merge_sortCA(ids_corridas)
+mergeSort(ids_corridas)
+t2 = time()
+print(ids_corridas, t2-t1)
+
+#Estrutura dos dicionários:
+# usuarios = {’468.791.579-55’: (’Lucas Soares Lopes’, 1, False),
+#               cpf : (nome, estrelas, ehMotorista)
+#veiculos = {’BJG-7G74’: (’Black’, ’821.833.773-30’),
+#               placa : (categoria , cpf_motorista)
+#corridas =  { 6: (’IXU-6J15’, ’468.791.579-55’, (2, 5, 2026), (0, 57), 11, 13),
+#               if : (placa, cpf_passageiro, data(dia,mes,aano), horario(hora, minuto), duracao, valor)
+
+#usemos essas terminologia pra facilitar o nosso trabalho para compreendermos facilmente o códgio
 
 #Saída de Dados
-nome_saida = "saida" + arq[7::]
-with open(nome_saida, 'w', encoding='utf-8') as exitFile:
-    categoria_atual = None
-    data_atual = None
-    
-    for ids in ids_corridas:
-        placa, cpf_passageiro, data, horario, duracao, valor = corridas[ids]
-        dia, mes, ano = data
-        hora, minuto = horario
-        hora_final = (hora * 60 + minuto + duracao) // 60
-        minuto_final = (hora * 60 + minuto + duracao) % 60
-        cat, cpf_m = veiculos[placa]
-        nome_m, stars, _ = usuarios[cpf_m]
-        nome_c, _, _ = usuarios[cpf_passageiro]
+
+
+
+
+#Estrutura de como deve ser a saída
         
-        if cat != categoria_atual:
-            if categoria_atual is not None:
-                print(file=exitFile)
-            print(f'CATEGORIA: {cat}\n', file=exitFile)
-            categoria_atual = cat
-            data_atual = None
-            
-        if data != data_atual:
-            print(f' {dia}/{mes}/{ano}\n', file=exitFile)
-            data_atual = data
-            
-        print(f'  Motorista: {nome_m} {stars * "*"}', file=exitFile)
-        print(f'  Cliente: {nome_c}', file=exitFile)
-        print(f'  Periodo: {hora}:{minuto} - {hora_final}:{minuto_final}', file=exitFile)
-        print(f'  Valor: R${valor:.2f}\n', file=exitFile)
-        
-    
-t2 = time()
-print(f'Arquivo gerado: {nome_saida}')
-print("tempo total: ", t2 - t1)
+'''
+CATEGORIA: Black
+\t02/05/2026
+\t\tMotorista: Antonio Barbosa Rodrigues ****
+\t\tCliente: Lucas Soares Lopes
+\t\tPeriodo: 12:10 - 12:36
+\t\tValor: R$28.00
+
+\t\tMotorista: Fernanda Santos Santana ****
+\t\tCliente: Lucas Soares Lopes
+\t\tPeriodo: 00:57 - 01:08
+\t\tValor: R$13.00   
+
+
+'''
--- testeIA.py
+++ testeIA.py
@@ -5,6 +5,7 @@
 
 import pickle
 from time import time
+from functools import lru_cache
 
 
 def chamado():
@@ -24,52 +25,39 @@ ids_corridas = [elem for elem in corridas]
 
 
 #mapeamento de prioridade das categorias:
-categoria_prioridade = {'Black': 1,'Comfort': 2,'Comum': 3,'Moto': 4}
+categoria_prioridade = {'Black': 1, 'Comfort': 2, 'Comum': 3, 'Moto': 4}
 
 
 #-----------------------------------------------------------------
+@lru_cache(maxsize=None)
+def chave(id_corrida):
+    """
+    Pré-computa e armazena em cache a tupla de ordenação de cada ID.
+    Calculada apenas uma vez por ID — reutilizada em todas as comparações.
+    """
+    placa, cpf_cliente, data, horario, duracao, valor = corridas[id_corrida]
+    dia, mes, ano = data
+
+    cat        = veiculos[placa][0]
+    cpf_mot    = veiculos[placa][1]
+    _, estrelas, _ = usuarios[cpf_mot]
+    nome_c, _, _   = usuarios[cpf_cliente]
+
+    return (
+        categoria_prioridade[cat],  # menor = mais prioritário
+        -ano, -mes, -dia,           # negativo → mais recente primeiro
+        -estrelas,                  # negativo → maior primeiro
+        nome_c,                     # alfabético
+        -valor                      # negativo → maior primeiro
+    )
+
+
 def comparar(id1, id2):
     """
     Retorna True se id1 deve vir antes de id2 na ordenação.
-    Critérios (em ordem de prioridade):
-      1. Categoria (Black > Comfort > Comum > Moto)
-      2. Data (mais recente primeiro)
-      3. Estrelas do motorista (maior primeiro)
-      4. Nome do cliente (ordem alfabética)
-      5. Valor da corrida (maior primeiro)
+    Agora usa apenas comparação de tuplas — O(1) após o cache aquecido.
     """
-    placa1, cpf_cliente1, data1, horario1, duracao1, valor1 = corridas[id1]
-    placa2, cpf_cliente2, data2, horario2, duracao2, valor2 = corridas[id2]
-    dia1, mes1, ano1 = data1
-    dia2, mes2, ano2 = data2
-
-    cat1 = veiculos[placa1][0]
-    cat2 = veiculos[placa2][0]
-    prio1 = categoria_prioridade[cat1]
-    prio2 = categoria_prioridade[cat2]
-    if prio1 != prio2:
-        return prio1 < prio2
-
-    if ano1 != ano2:
-        return ano1 > ano2
-    if mes1 != mes2:
-        return mes1 > mes2
-    if dia1 != dia2:
-        return dia1 > dia2
-
-    cpf_motorista1 = veiculos[placa1][1]
-    cpf_motorista2 = veiculos[placa2][1]
-    _, estrelas1, _ = usuarios[cpf_motorista1]
-    _, estrelas2, _ = usuarios[cpf_motorista2]
-    if estrelas1 != estrelas2:
-        return estrelas1 > estrelas2
-
-    nome_cliente1, _, _ = usuarios[cpf_cliente1]
-    nome_cliente2, _, _ = usuarios[cpf_cliente2]
-    if nome_cliente1 != nome_cliente2:
-        return nome_cliente1 < nome_cliente2
-
-    return valor1 > valor2
+    return chave(id1) < chave(id2)
 
 
 #-----------------------------------------------------------------
@@ -77,12 +65,12 @@ def comparar(id1, id2):
 def insertion_sort(l, inicio, fim):
 
     for i in range(inicio + 1, fim):
-        chave = l[i]
+        chave_i = l[i]
         j = i - 1
-        while j >= inicio and comparar(chave, l[j]):
+        while j >= inicio and comparar(chave_i, l[j]):
             l[j + 1] = l[j]
             j -= 1
-        l[j + 1] = chave
+        l[j + 1] = chave_i
 
 #-----------------------------------------------------------------
 
@@ -101,15 +89,13 @@ def merge(l, lEsq, lDir):
             j += 1
         k += 1
 
-
     #copia o restante de lEsq
     while i < len(lEsq):
         l[k] = lEsq[i]
         i += 1
         k += 1
 
-
-    #copia o restante de lDir 
+    #copia o restante de lDir
     while j < len(lDir):
         l[k] = lDir[j]
         j += 1
@@ -118,14 +104,14 @@ def merge(l, lEsq, lDir):
 
 def merge_sortCA(l):
 
-    if len(l) <= 30:
+    if len(l) <= 32:            # limiar ligeiramente maior → menos chamadas recursivas
         insertion_sort(l, 0, len(l))
         return
-    
+
     meio = len(l) // 2
-    lEsq = l[:meio] #cópia da primeira metade
-    lDir = l[meio:] #cópia da segunda metade
-    
+    lEsq = l[:meio]             #cópia da primeira metade
+    lDir = l[meio:]             #cópia da segunda metade
+
     merge_sortCA(lEsq)
     merge_sortCA(lDir)
     merge(l, lEsq, lDir)
@@ -140,35 +126,37 @@ merge_sortCA(ids_corridas)
 nome_saida = "saida" + arq[7::]
 with open(nome_saida, 'w', encoding='utf-8') as exitFile:
     categoria_atual = None
-    data_atual = None
-    
+    data_atual      = None
+
     for ids in ids_corridas:
         placa, cpf_passageiro, data, horario, duracao, valor = corridas[ids]
-        dia, mes, ano = data
-        hora, minuto = horario
-        hora_final = (hora * 60 + minuto + duracao) // 60
-        minuto_final = (hora * 60 + minuto + duracao) % 60
-        cat, cpf_m = veiculos[placa]
+        dia, mes, ano   = data
+        hora, minuto    = horario
+        total_min       = hora * 60 + minuto + duracao
+        hora_final, minuto_final = divmod(total_min, 60)   # substitui // e %
+        cat, cpf_m      = veiculos[placa]
         nome_m, stars, _ = usuarios[cpf_m]
-        nome_c, _, _ = usuarios[cpf_passageiro]
-        
+        nome_c, _, _    = usuarios[cpf_passageiro]
+
         if cat != categoria_atual:
             if categoria_atual is not None:
-                print(file=exitFile)
-            print(f'CATEGORIA: {cat}\n', file=exitFile)
+                exitFile.write('\n')
+            exitFile.write(f'CATEGORIA: {cat}\n\n')
             categoria_atual = cat
-            data_atual = None
-            
+            data_atual      = None
+
         if data != data_atual:
-            print(f' {dia}/{mes}/{ano}\n', file=exitFile)
+            exitFile.write(f' {dia}/{mes}/{ano}\n\n')
             data_atual = data
-            
-        print(f'  Motorista: {nome_m} {stars * "*"}', file=exitFile)
-        print(f'  Cliente: {nome_c}', file=exitFile)
-        print(f'  Periodo: {hora}:{minuto} - {hora_final}:{minuto_final}', file=exitFile)
-        print(f'  Valor: R${valor:.2f}\n', file=exitFile)
-        
-    
+
+        # Uma única chamada write por corrida — menos syscalls de I/O
+        exitFile.write(
+            f'  Motorista: {nome_m} {"*" * stars}\n'
+            f'  Cliente: {nome_c}\n'
+            f'  Periodo: {hora}:{minuto} - {hora_final}:{minuto_final}\n'
+            f'  Valor: R${valor:.2f}\n\n'
+        )
+
 t2 = time()
 print(f'Arquivo gerado: {nome_saida}')
 print("tempo total: ", t2 - t1)
